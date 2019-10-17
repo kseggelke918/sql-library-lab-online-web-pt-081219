46b66085364d77a95bff3760ab589585982408bc
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 19:31:41 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,7 +10,7 @@ INSERT INTO authors (name) VALUES
 ("author1"),
 ("author2");
 
-INSERT INTO series (title, author_id, subgenre_id) VALUES 
+INSERT INTO books (title, author_id, subgenre_id) VALUES 
 ("title1", 1, 1),
 ("title2", 2, 2),
 ("title3", 3, 3);
