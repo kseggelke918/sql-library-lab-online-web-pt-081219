21c86d979f7627e6158b780980eab5bdafe3fec9
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 19:27:58 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,4 +2,6 @@ INSERT INTO series (title, author_id, genre_id) VALUES
 ("title1", 2, 1),
 ("title2", 1, 2);
 
-INSERT INTO subgenres ()
+INSERT INTO subgenres (name) VALUES 
+("fantasy"),
+("horror");
