b43f2c177dec73f8b72f6d53838d6fb1b4ec39ad
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 19:57:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species) FROM characters
-  GROUP BY species;"
+  GROUP BY species
+  ORDER BY COUNT(species);"
 end
 
 def select_name_and_series_subgenres_of_authors
