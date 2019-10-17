fe188ac859f54c2d86aa772b0353f8daa87817fa
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 20:00:30 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT author.name, subgenre.name FROM series 
+  "SELECT authors.name, subgenres.name FROM series 
   INNER JOIN subgenres ON subgenres.id = series.subgere_id
   INNER JOIN authors ON authors.id = series.author_id;"
 end
