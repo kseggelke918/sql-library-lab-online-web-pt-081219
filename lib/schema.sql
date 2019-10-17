befa6328c69ae28665d5a3ad7af5e50c7df5c1e2
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 19:24:55 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -19,6 +19,15 @@ CREATE TABLE series (
  series_id INTEGER);
  
  CREATE TABLE characters (
- id INTEGER PRIMARY KEY,);
+ id INTEGER PRIMARY KEY,
+ name TEXT,
+ motto TEXT, 
+ species TEXT,
+ author_id INTEGER);
+ 
+ CREATE TABLE character_books (
+ id INTEGER PRIMARY KEY,
+ character_id INTEGER,
+ book_id INTEGER);
  
  
