e996ea6dea0439f97bee57c3a616f7ae87dcdd87
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 19:22:04 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,3 +1,24 @@
 CREATE TABLE series (
  id INTEGER PRIMARY KEY, 
- title TEXT)
+ title TEXT,
+ author_id INTEGER,
+ sub_genre_id INTEGER);
+ 
+ CREATE TABLE sub_genres(
+ id INTEGER PRIMARY KEY,
+ name TEXT);
+ 
+ CREATE TABLE authors (
+ id INTEGER PRIMARY KEY,
+ name TEXT);
+ 
+ CREATE TABLE books (
+ id INTEGER PRIMARY KEY,
+ title TEXT,
+ year INTEGER,
+ series_id INTEGER);
+ 
+ CREATE TABLE characters (
+ id INTEGER PRIMARY KEY,);
+ 
+ 
