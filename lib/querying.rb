288207b1ad70e939e7edc330091ca491604998ca
Katie Seggelke <katie.seggelke@gmail.com>
2019-10-17 20:03:44 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,7 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series 
   INNER JOIN characters ON characters.author_id = series.author_id
   GROUP BY characters.species 
-  ORDER BY COUNT(characters.species);"
+  ORDER BY COUNT(characters.species) LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
